1352616dc1ce7110a94807a36b3a6534cc5be507
Manu2402 <solanomanuel002@gmail.com>
2025-07-03 09:53:49 +0200

    Base project commit

---
+++ SimpleSteamSession/Source/SimpleSteamSession/SimpleSteamSession.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleSteamSession.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleSteamSession, "SimpleSteamSession" );
+ 
---
+++ SimpleSteamSession/Source/SimpleSteamSession/SimpleSteamSessionGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleSteamSessionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASimpleSteamSessionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASimpleSteamSessionGameMode();
+};
+
+
+
